85260eab414c4f75cc1775765ca2fdc89addc705
Sam Sutcliffe <sam.sutcliffe@.dur.ac.uk>
2024-01-31 00:23:32 +0000

    Updated joss

--- joss.lisp
+++ joss.lisp
@@ -37,17 +37,17 @@
 
                 :ft 200d3
                 :fc 500d3
-                :friction-angle 40d0
+                :friction-angle 60d0
 
                 :fracture-energy 3000d0
-                :initiation-stress 90d3
-                :delay-time 1d0
+                :initiation-stress 20d3
+                :delay-time 1d1
                 :ductility 4d0
                 ;; :compression-ratio 8d0
 
                 :critical-damage 1.0d0;0.999d0
-                :damage-domain-rate 0.0d0;This slider changes how GIMP update turns to uGIMP under damage
-                :local-length (* 0.1d0 (sqrt 7))
+                :damage-domain-rate 0.9d0;This slider changes how GIMP update turns to uGIMP under damage
+                :local-length (* 0.25d0 (sqrt 7))
                 ;; :local-length-damaged (* 0.1d0 (sqrt 7))
                 ;; :local-length-damaged 1d0
                 :local-length-damaged 10d-10
@@ -55,9 +55,9 @@
                 ;; 'cl-mpm/particle::particle-mc
                 ;; :E 1d9
                 ;; :nu 0.35d0
-                :psi (* 00d0 (/ pi 180))
+                :psi (* 0d0 (/ pi 180))
                 :phi (* 40d0 (/ pi 180))
-                :c 1000d3
+                :c 500d3
                 ;; :c 1d6
 
                 ;; 'cl-mpm/particle::particle-vm
@@ -165,12 +165,12 @@
     ))
 
 (defun setup ()
-  (let* ((mesh-size 0.25)
+  (let* ((mesh-size 0.5)
          (mps-per-cell 2)
-         (shelf-height 15.4)
+         (shelf-height 15.5)
          (soil-boundary 3)
-         (shelf-aspect 0.75)
-         (runout-aspect 0.25)
+         (shelf-aspect 2.0)
+         (runout-aspect 1.50)
          (shelf-length (* shelf-height shelf-aspect))
          (domain-length (+ shelf-length (* runout-aspect shelf-height)))
          (shelf-height (+ shelf-height soil-boundary))
@@ -202,17 +202,16 @@
                (and
                 (> (magicl:tref (cl-mpm/particle:mp-position mp) 0 0)
                    (- shelf-length
-                      3d0
-                      ;; (* 1d0 shelf-height)
+                      (* 1d0 shelf-height)
                       ))
                 (< (magicl:tref (cl-mpm/particle:mp-position mp) 0 0)
                    shelf-length)
                 (> (magicl:tref (cl-mpm/particle:mp-position mp) 1 0)
                    soil-boundary
                    )
-                (< (magicl:tref (cl-mpm/particle:mp-position mp) 1 0)
-                   (- shelf-height 7d0)
-                   )
+                ;(< (magicl:tref (cl-mpm/particle:mp-position mp) 1 0)
+                ;   (- shelf-height 7d0)
+                ;   )
                 )
              dir
              )))))
@@ -269,7 +268,7 @@
                                       1d0)
                                   )))
 
-     (setf cl-mpm::*max-split-depth* 6)
+     (setf cl-mpm::*max-split-depth* 3)
 
      ;; (let ((ratio 1.0d0))
      ;;   (cl-mpm/setup::damage-sdf *sim* (lambda (p) (cl-mpm/setup::line-sdf
@@ -307,7 +306,7 @@
   (let* ((target-time 1d2)
          (dt (cl-mpm:sim-dt *sim*))
          (substeps (floor target-time dt))
-         (dt-scale 0.9d0))
+         (dt-scale 1.0d0))
 
     (cl-mpm::update-sim *sim*)
     (multiple-value-bind (dt-e substeps-e) (cl-mpm:calculate-adaptive-time *sim* target-time :dt-scale dt-scale)
@@ -325,6 +324,7 @@
                         (setf (cl-mpm:sim-damping-factor *sim*) (* 1d-4 ms)))
                        )
                      (format t "Step ~d ~%" steps)
+                     (format t "MPs ~d ~%" (length (cl-mpm:sim-mps *sim*)))
                      (cl-mpm/output:save-vtk (merge-pathnames (format nil "output/sim_~5,'0d.vtk" *sim-step*)) *sim*)
                      (time
                       (dotimes (i substeps);)
@@ -343,8 +343,8 @@
     ;(setup)
 
     ;;Square
-    (let ((dsize (floor (sqrt (cl-mpi:mpi-comm-size)))))
-      (setf (cl-mpm/mpi::mpm-sim-mpi-domain-count *sim*) (list dsize dsize 1)))
+    (let ((dsize (floor (cl-mpi:mpi-comm-size))))
+      (setf (cl-mpm/mpi::mpm-sim-mpi-domain-count *sim*) (list dsize 1 1)))
     ;;Setup domain deomposition
     ;(let ((dsize (floor (cl-mpi:mpi-comm-size) 2)))
     ;  (setf (cl-mpm/mpi::mpm-sim-mpi-domain-count *sim*) (list dsize 2 1)))
@@ -384,8 +384,8 @@
          (target-time 1d1)
          (target-time-original target-time)
          (mass-scale (cl-mpm::sim-mass-scale *sim*))
-         (collapse-target-time 1d0)
-         (collapse-mass-scale 1d3)
+         (collapse-target-time 0.1d0)
+         (collapse-mass-scale 1d0)
          (dt (cl-mpm:sim-dt *sim*))
          (substeps (floor target-time dt))
          (settle-steps 10)
@@ -429,11 +429,10 @@
                        (when (= rank 0)
                          (format t "Energy estimate: ~E~%" energy-estimate))
                        (when (>= steps damp-steps)
-                         (setf (cl-mpm:sim-damping-factor *sim*) 1d-6 
-                               target-time 1d1))
+                         (setf (cl-mpm:sim-damping-factor *sim*) 0d-6))
                        (when (>= steps settle-steps)
                          (setf (cl-mpm::sim-enable-damage *sim*) t)  
-                         (when (> energy-estimate 1d-5)
+                         (when (> energy-estimate 1d-4)
                            (progn
                              (when (= rank 0)
                                (format t "Collapse timestep~%"))
